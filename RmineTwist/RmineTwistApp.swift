c0765518deb7647ebdabd35190f94f3e7cf194a9
이민규 <67575226+suslmk-lee@users.noreply.github.com>
2024-08-12 10:05:15 +0900

    Initial Commit

---
+++ RmineTwist/RmineTwistApp.swift
@@ -0,0 +1,17 @@
+//
+//  RmineTwistApp.swift
+//  RmineTwist
+//
+//  Created by Minkyu Lee on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RmineTwistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
