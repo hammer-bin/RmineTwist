8191c39b4b922031088a09690ceb2c60e91a7c2c
이민규 <mctlmk@gmail.com>
2024-08-16 07:00:51 +0900

    commit

---
+++ RmineTwist/Extensions/PreviewProvider.swift
@@ -0,0 +1,18 @@
+//
+//  PreviewProvider.swift
+//  RmineTwist
+//
+//  Created by minkyuLee on 8/15/24.
+//
+
+import SwiftUI
+
+struct PreviewProvider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreviewProvider()
+}
